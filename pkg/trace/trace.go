203fc50f380bea3af1257f64cc1654951c02061e
Elvis de Freitas <edigitalb@gmail.com>
2017-10-21 00:26:23 -0200

    turn trace function cross golang version

--- pkg/trace/trace.go
+++ pkg/trace/trace.go
@@ -15,15 +15,11 @@ import (
 var rx, _ = regexp.Compile("\\.func\\d+$")
 func GetCallerFunctionName(backLevel int) string {
 
-	var pc, tryAgain = make([]uintptr, backLevel + 10), true
+	var pc, tryAgain = make([]uintptr, backLevel + 3), true
 	runtime.Callers(0, pc) // pass skip did different results for different go version eg 1.7 and 1.9
 
-	pc = pc[backLevel + 2:]
-	//for i:=0; i < len(pc); i++ {
-	//	fmt.Printf("i=%d, lvl=%d, fn=%s\n", i, backLevel, runtime.FuncForPC(pc[i]).Name())
-	//}
-
 	var fn *runtime.Func
+	pc = pc[backLevel + 2:]
 	for i:=0; i < len(pc) && tryAgain; i++ {
 		fn = runtime.FuncForPC(pc[i])
 		tryAgain = rx.MatchString(fn.Name())
