562f70ed31190fa72ad1dfdef3ee409cb2ddd884
Elvis de Freitas <edigitalb@gmail.com>
2017-10-21 00:36:30 -0200

    adding travis and adding buildf

--- pkg/trace/trace.go
+++ pkg/trace/trace.go
@@ -12,10 +12,10 @@ import (
 // 1 -> returns the current caller parent
 // etc.
 //
-var rx, _ = regexp.Compile("\\.func\\d+$")
+var rx, _ = regexp.Compile(`\.func\d+$`)
 func GetCallerFunctionName(backLevel int) string {
 
-	var pc, tryAgain = make([]uintptr, backLevel + 3), true
+	var pc, tryAgain = make([]uintptr, backLevel + 4), true
 	runtime.Callers(0, pc) // pass skip did different results for different go version eg 1.7 and 1.9
 
 	var fn *runtime.Func
